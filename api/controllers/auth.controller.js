38c2e11a512684faced4e41315d1123de0b34474
i-sphinx <sphinxatlinux@gmail.com>
2024-06-21 17:01:35 +0530

    added functionality for error handling

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,8 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
   const { username, password, email } = req.body;
   if (
     !username ||
@@ -11,7 +12,7 @@ export const signup = async (req, res) => {
     password === "" ||
     email === ""
   ) {
-    return res.status(400).json({ message: "All Fields Are Required" });
+    next(errorHandler(400, "All Fields Are Required"));
   }
   const hashedPassword = bcryptjs.hashSync(password, 10);
 
@@ -24,6 +25,6 @@ export const signup = async (req, res) => {
     await newUser.save();
     res.json("Signup Succesful");
   } catch (error) {
-    return res.status(500).json({ message: error.message });
+    next(error);
   }
 };

308e7fcf714330e4c45ac7f8fab26215bea45440
i-sphinx <sphinxatlinux@gmail.com>
2024-06-22 10:25:29 +0530

    added signin API route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, password, email } = req.body;
@@ -12,8 +13,9 @@ export const signup = async (req, res, next) => {
     password === "" ||
     email === ""
   ) {
-    next(errorHandler(400, "All Fields Are Required"));
+    return next(errorHandler(400, "All Fields Are Required"));
   }
+
   const hashedPassword = bcryptjs.hashSync(password, 10);
 
   const newUser = new User({
@@ -21,9 +23,40 @@ export const signup = async (req, res, next) => {
     password: hashedPassword,
     email: email,
   });
+
   try {
     await newUser.save();
-    res.json("Signup Succesful");
+    res.json("Signup Successful");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email || !password || email === "" || password === "") {
+    return next(errorHandler(400, "All fields are required"));
+  }
+
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(400, "User not found"));
+    }
+
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(400, "Invalid Password"));
+    }
+
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_KEY);
+    const { password: pass, ...rest } = validUser._doc;
+    res
+      .status(200)
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .json(rest);
   } catch (error) {
     next(error);
   }

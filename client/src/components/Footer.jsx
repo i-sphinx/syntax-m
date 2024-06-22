8e56078d9e32bc7d626387031b75276ea784ddf5
i-sphinx <sphinxatlinux@gmail.com>
2024-06-22 09:39:44 +0530

    layout fixes

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,25 +2,70 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer className="bg-gray-900 text-gray-300 border-t-2 border-gray-700 border-solid py-8 text-lg font-medium dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 sm:text-base">
+    <footer className="border-t-2 py-4 sm:py-8 text-sm sm:text-base font-medium bg-light-background dark:bg-dark-background text-light-text dark:text-dark-text">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex justify-between items-center">
-          <span>{new Date().getFullYear()} &copy; All Right Reserved</span>
-          <div className="flex items-center">
-            Built With{" "}
+        <div className="flex flex-col sm:flex-row justify-between items-center">
+          {/* Element 1 */}
+          <div className="flex flex-col sm:flex-row items-center text-center sm:text-left mb-4 sm:mb-0">
+            <span className="mb-2 sm:mb-0 sm:mr-4">
+              {new Date().getFullYear()} &copy; All Rights Reserved
+            </span>
+          </div>
+
+          {/* Element 2 */}
+          <div className="flex justify-center sm:justify-start items-center mb-4 sm:mb-0">
+            Built With
             <span className="text-blue-600 dark:text-blue-200 text-2xl px-1">
               &#9825;
-            </span>{" "}
-            By &nbsp; Prince
+            </span>
+            By Prince
           </div>
-          <a
-            href="mailto:sphinxatlinux@gmail.com"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-blue-600 dark:text-blue-200 hover:underline"
-          >
-            Say Hello
-          </a>
+
+          {/* Element 3 */}
+          <div className="flex justify-center sm:justify-end border-t-2 sm:border-none pt-2 sm:pt-0">
+            <ul className="flex space-x-5">
+              <li>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  className="w-6 h-6 text-light-secondary dark:text-dark-secondary"
+                >
+                  <path d="M12 2.163c3.204 0 3.584.012 4.85.07 1.366.062 2.633.328 3.608 1.302.976.976 1.24 2.243 1.302 3.609.058 1.265.07 1.645.07 4.849 0 3.204-.012 3.584-.07 4.85-.062 1.366-.328 2.633-1.302 3.608-.976.976-2.243 1.24-3.609 1.302-1.265.058-1.645.07-4.849.07-3.204 0-3.584-.012-4.85-.07-1.366-.062-2.633-.328-3.608-1.302-.976-.976-1.24-2.243-1.302-3.609C2.175 15.584 2.163 15.204 2.163 12c0-3.204.012-3.584.07-4.85.062-1.366.328-2.633 1.302-3.608.976-.976 2.243-1.24 3.609-1.302 1.265-.058 1.645-.07 4.849-.07M12 0C8.741 0 8.332.012 7.052.07 5.76.128 4.675.392 3.703 1.364c-.973.973-1.237 2.058-1.295 3.35C2.012 6.833 2 7.242 2 10.5c0 3.258.012 3.667.07 4.947.058 1.292.322 2.377 1.295 3.35.973.973 2.058 1.237 3.35 1.295 1.292.058 1.701.07 4.947.07 3.258 0 3.667-.012 4.947-.07 1.292-.058 2.377-.322 3.35-1.295.973-.973 1.237-2.058 1.295-3.35.058-1.292.07-1.701.07-4.947 0-3.258-.012-3.667-.07-4.947-.058-1.292-.322-2.377-1.295-3.35-.973-.973-2.058-1.237-3.35-1.295C15.667.012 15.258 0 12 0zm0 5.838a6.162 6.162 0 1 0 0 12.324A6.162 6.162 0 0 0 12 5.838zm0 10.124a3.962 3.962 0 1 1 0-7.924 3.962 3.962 0 0 1 0 7.924zm6.406-11.2a1.44 1.44 0 1 0-2.88 0 1.44 1.44 0 0 0 2.88 0z" />
+                </svg>
+              </li>
+              <li>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  className="w-6 h-6 text-light-secondary dark:text-dark-secondary"
+                >
+                  <path d="M12 2C6.477 2 2 6.477 2 12c0 4.418 2.865 8.167 6.839 9.49.5.092.682-.217.682-.482 0-.237-.009-.868-.014-1.703-2.782.604-3.369-1.34-3.369-1.34-.454-1.154-1.11-1.461-1.11-1.461-.907-.62.069-.608.069-.608 1.002.07 1.53 1.03 1.53 1.03.892 1.528 2.341 1.087 2.91.832.092-.647.35-1.087.636-1.338-2.22-.252-4.555-1.11-4.555-4.943 0-1.09.39-1.98 1.03-2.68-.103-.253-.447-1.27.098-2.646 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0 1 12 6.844a9.55 9.55 0 0 1 2.508.336c1.91-1.296 2.75-1.026 2.75-1.026.546 1.376.202 2.393.1 2.646.64.7 1.03 1.59 1.03 2.68 0 3.843-2.337 4.688-4.566 4.935.36.31.68.923.68 1.86 0 1.344-.012 2.427-.012 2.756 0 .267.18.578.688.48A10.003 10.003 0 0 0 22 12c0-5.523-4.477-10-10-10z" />
+                </svg>
+              </li>
+              <li>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  className="w-6 h-6 text-light-secondary dark:text-dark-secondary"
+                >
+                  <path d="M12 2c-5.523 0-10 4.477-10 10 0 4.109 2.468 7.674 6 9.192-.083-.779-.157-1.975.033-2.828.172-.744 1.115-4.73 1.115-4.73s-.285-.572-.285-1.415c0-1.327.77-2.318 1.728-2.318.814 0 1.207.611 1.207 1.342 0 .818-.521 2.041-.791 3.174-.228.963.482 1.748 1.428 1.748 1.714 0 3.029-1.807 3.029-4.413 0-2.306-1.656-3.922-4.03-3.922-2.743 0-4.35 2.055-4.35 4.18 0 .826.318 1.714.718 2.197.08.095.092.178.068.274-.075.3-.245.963-.278 1.096-.045.18-.145.221-.334.134-1.247-.58-2.028-2.397-2.028-3.863 0-3.138 2.274-6.022 6.566-6.022 3.444 0 6.122 2.454 6.122 5.742 0 3.425-2.157 6.179-5.153 6.179-1.004 0-1.95-.523-2.274-1.144l-.621 2.368c-.223.86-.83 1.937-1.238 2.595.93.288 1.916.445 2.938.445 5.523 0 10-4.477 10-10 0-5.522-4.477-10-10-10z" />
+                </svg>
+              </li>
+              <li>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  className="w-6 h-6 text-light-secondary dark:text-dark-secondary"
+                >
+                  <path d="M19.769 3H4.231A1.232 1.232 0 0 0 3 4.231v15.538A1.232 1.232 0 0 0 4.231 21h15.538A1.232 1.232 0 0 0 21 19.769V4.231A1.232 1.232 0 0 0 19.769 3zM8.594 18.306H5.779v-7.4h2.815v7.4zM7.187 9.426a1.65 1.65 0 1 1 .002-3.299 1.65 1.65 0 0 1-.002 3.299zm11.119 8.88h-2.814v-3.722c0-.887-.016-2.027-1.237-2.027-1.237 0-1.426.967-1.426 1.967v3.782h-2.815v-7.4h2.703v1.011h.037c.376-.712 1.296-1.463 2.664-1.463 2.85 0 3.376 1.877 3.376 4.319v3.533z" />
+                </svg>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </footer>
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -46,33 +46,32 @@ const Signup = () => {
   };
 
   return (
-    <div className="min-h-screen pt-32 overflow-hidden w-full bg-light-background dark:bg-gray-900 text-light-text dark:text-dark-text">
-      <div className="flex flex-col md:flex-row mx-5 sm:max-w-[80%] gap-4 items-center sm:mx-auto">
-        <div className="flex-1 ">
+    <div className="min-h-screen flex items-center justify-center bg-light-background dark:bg-gray-900 text-light-text dark:text-dark-text">
+      <div className="flex flex-col md:flex-row w-full max-w-4xl p-8 gap-8 bg-white dark:bg-gray-800 rounded-lg shadow-lg">
+        <div className="flex-1 flex flex-col items-center justify-center">
           <Link
             to="/"
-            className="text-sm sm:text-2xl font-semibold shadow-blue-400 shadow-2xl mx-20 sm:mx-0"
+            className="text-3xl font-semibold text-blue-500 dark:text-blue-300 mb-4"
           >
-            <span className="px-4 sm:py-1 bg-gradient-to-br from-sky-300 via-sky-400 to-blue-500 rounded-br-full rounded-tl-full text-2xl sm:text-3xl uppercase">
+            <span className="bg-gradient-to-br from-sky-300 via-sky-400 to-blue-500 bg-clip-text text-transparent">
               Syntax
             </span>
-            <span className="underline p-1">Sunrise</span>
+            <span className="underline">Sunrise</span>
           </Link>
-          <p className="mt-5 p-5">
+          <p className="text-center px-6">
             Get started with these custom Tailwind CSS form components to gather
             information from your users using input text elements, checkboxes,
             radios, textareas, selects, file uploads, toggle switches, and more.
           </p>
         </div>
 
-        {/* right side ...... */}
-
-        <div className="flex-1 mb-10">
+        <div className="flex-1">
           <form
-            className="sm:max-w-lg mx-auto bg-gray-200 dark:bg-gray-800 text-gray-800 dark:text-gray-200 rounded-2xl p-12"
+            className="bg-gray-200 dark:bg-gray-800 text-gray-800 dark:text-gray-200 rounded-2xl p-8"
             onSubmit={handleSubmit}
           >
-            <div className="mb-5">
+            <h2 className="text-2xl font-semibold text-center mb-6">Sign Up</h2>
+            <div className="mb-4">
               <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
                 Username
               </label>
@@ -84,7 +83,7 @@ const Signup = () => {
                 onChange={handleChange}
               />
             </div>
-            <div className="mb-5">
+            <div className="mb-4">
               <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
                 Password
               </label>
@@ -96,7 +95,7 @@ const Signup = () => {
                 onChange={handleChange}
               />
             </div>
-            <div className="mb-5">
+            <div className="mb-4">
               <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
                 Email
               </label>
@@ -111,47 +110,44 @@ const Signup = () => {
 
             <button
               type="submit"
-              className="flex text-white bg-light-primary dark:bg-dark-primary hover:bg-light-secondary dark:hover:bg-dark-secondary focus:ring-4 focus:outline-none focus:ring-light-secondary dark:focus:ring-dark-secondary font-medium rounded-lg text-xs md:text-sm px-4 py-2"
-              disabled={loading} // Disable button during loading
+              className="w-full text-white bg-light-primary dark:bg-dark-primary hover:bg-light-secondary dark:hover:bg-dark-secondary focus:ring-4 focus:outline-none focus:ring-light-secondary dark:focus:ring-dark-secondary font-medium rounded-lg text-sm px-5 py-2.5 text-center disabled:opacity-50"
+              disabled={loading}
             >
               {loading ? (
-                <>
-                  <div class="flex items-center justify-center w-auto h-auto rounded-lg">
-                    <div>
-                      <svg
-                        aria-hidden="true"
-                        class="w-5 h-5 text-gray-200 animate-spin dark:text-gray-100 fill-none dark:border-2 border-2"
-                        viewBox="0 0 100 101"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                          fill="currentColor"
-                        />
-                        <path
-                          d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                          fill="currentFill"
-                        />
-                      </svg>
-                    </div>
-                    <span class="pl-3">Loading...</span>
-                  </div>
-                </>
+                <div className="flex items-center justify-center">
+                  <svg
+                    aria-hidden="true"
+                    className="w-5 h-5 text-gray-200 animate-spin dark:text-gray-100 fill-current"
+                    viewBox="0 0 100 101"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                      fill="currentColor"
+                    />
+                    <path
+                      d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                      fill="currentFill"
+                    />
+                  </svg>
+                  <span className="pl-3">Loading...</span>
+                </div>
               ) : (
                 "Sign Up"
               )}
             </button>
-            <div className="mt-5 text-sm">
-              <span>Have An Account ? </span>
-              <Link to="/Signin" className="text-blue-400">
+
+            <div className="mt-6 text-center">
+              <span>Already have an account? </span>
+              <Link to="/Signin" className="text-blue-500 hover:underline">
                 Sign In
               </Link>
             </div>
 
             {errorMessage && (
               <div
-                className="p-4 mb-4 mt-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-700 dark:text-red-400"
+                className="mt-4 text-sm text-red-600 bg-red-100 rounded-lg p-3 dark:bg-red-800 dark:text-red-200"
                 role="alert"
               >
                 {errorMessage}

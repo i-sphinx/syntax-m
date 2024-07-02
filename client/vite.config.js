5eb979c8b27c588e4125c2b53a36be98d591108d
i-sphinx <sphinxatlinux@gmail.com>
2024-07-02 15:36:01 +0530

    removd some files

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,4 +13,7 @@ export default defineConfig({
   },
 
   plugins: [react()],
+  build: {
+    outDir: "dist",
+  },
 });
